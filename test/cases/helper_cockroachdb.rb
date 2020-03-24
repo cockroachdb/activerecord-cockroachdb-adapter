d81aa91a8c3d953f297c76d3ee795c73afc0fa2e
Ali Ibrahim <aibrahim2k2@gmail.com>
2020-03-24 15:40:19 -0400

    Add more testing options
      * In addition to runnning the ActiveRecord test suite, we can now run tests
        specific to the CockroachDB Adapter.
      * ONLY_TEST_AR and ONLY_TEST_COCKROACHDB environment variables can be set to
        run only the ActiveRecord test suite or CockroachDB Adapter test suite
        respectively.
      * Environment variable TEST_FILES can be used to run specific CockroachDB
        Adapter tests.
      * Also turn on debugging by default in the test environment.

--- test/cases/helper_cockroachdb.rb
+++ test/cases/helper_cockroachdb.rb
@@ -1,2 +1,5 @@
 require 'bundler/setup'
 Bundler.require :default, :development
+
+# Turn on debugging for the test environment
+ENV['DEBUG_COCKROACHDB_ADAPTER'] = "1"
--- test/support/rake_helpers.rb
+++ test/support/rake_helpers.rb
@@ -1,22 +1,52 @@
-COCKROACHDB_HELPER = 'test/cases/helper_cockroachdb'
+COCKROACHDB_TEST_HELPER = 'test/cases/helper_cockroachdb'
 
-def env_ar_test_files
+def test_files
+  env_activerecord_test_files ||
+    env_cockroachdb_test_files ||
+    only_activerecord_test_files ||
+    only_cockroachdb_test_files ||
+    all_test_files
+end
+
+def env_activerecord_test_files
   return unless ENV['TEST_FILES_AR'] && !ENV['TEST_FILES_AR'].empty?
 
   @env_ar_test_files ||= ENV['TEST_FILES_AR'].
     split(',').
     map { |file| File.join ARTest::CockroachDB.root_activerecord, file.strip }.
-    sort
+    sort.
+    prepend(COCKROACHDB_TEST_HELPER)
 end
 
-def ar_cases
-  @ar_cases ||= Dir.
+def env_cockroachdb_test_files
+  return unless ENV['TEST_FILES'] && !ENV['TEST_FILES'].empty?
+
+  @env_test_files ||= ENV['TEST_FILES'].split(',').map(&:strip)
+end
+
+def only_activerecord_test_files
+  return unless ENV['ONLY_TEST_AR']
+  activerecord_test_files
+end
+
+def only_cockroachdb_test_files
+  return unless ENV['ONLY_TEST_COCKROACHDB']
+  cockroachdb_test_files
+end
+
+def all_test_files
+  activerecord_test_files + cockroachdb_test_files
+end
+
+def activerecord_test_files
+  Dir.
     glob("#{ARTest::CockroachDB.root_activerecord}/test/cases/**/*_test.rb").
     reject{ |x| x =~ /\/adapters\/mysql2\// }.
     reject{ |x| x =~ /\/adapters\/sqlite3\// }.
-    sort
+    sort.
+    prepend(COCKROACHDB_TEST_HELPER)
 end
 
-def test_files
-  (env_ar_test_files || ar_cases).prepend(COCKROACHDB_HELPER)
+def cockroachdb_test_files
+  Dir.glob('test/cases/**/*_test.rb')
 end
