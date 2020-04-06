8149946c35e9d64b1c94498470ed7e783cf46547
Ali Ibrahim <aibrahim2k2@gmail.com>
2020-04-06 13:53:16 -0400

    Skip PostgreSQL geometric tests as geometric types are not supported in CockroachDB

---
+++ test/excludes/PostgreSQLGeometricLineTest.rb
@@ -0,0 +1,3 @@
+exclude :test_geometric_line_type, "Geometric types are not supported in CockroachDB. See https://github.com/cockroachdb/cockroach/issues/21286."
+exclude :test_alternative_format_line_type, "Geometric types are not supported in CockroachDB. See https://github.com/cockroachdb/cockroach/issues/21286."
+exclude :test_schema_dumping_for_line_type, "Geometric types are not supported in CockroachDB. See https://github.com/cockroachdb/cockroach/issues/21286."
---
+++ test/excludes/PostgresqlGeometricTest.rb
@@ -0,0 +1,4 @@
+exclude :test_geometric_types, "Geometric types are not supported in CockroachDB. See https://github.com/cockroachdb/cockroach/issues/21286."
+exclude :test_alternative_format, "Geometric types are not supported in CockroachDB. See https://github.com/cockroachdb/cockroach/issues/21286."
+exclude :test_geometric_function, "Geometric types are not supported in CockroachDB. See https://github.com/cockroachdb/cockroach/issues/21286."
+exclude :test_schema_dumping, "Geometric types are not supported in CockroachDB. See https://github.com/cockroachdb/cockroach/issues/21286."
