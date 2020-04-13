8d7a1e5f17c9d2a2a4b205b57c609cd40af43c29
Marla Brizel Zeschin <marlabrizel@gmail.com>
2020-04-13 11:10:23 -0600

    Ignore UpdateableViewTest

---
+++ test/excludes/UpdateableViewTest.rb
@@ -0,0 +1,3 @@
+exclude :test_update_record_to_fail_view_conditions, "Updateable views are not currently supported, see https://github.com/cockroachdb/cockroach/issues/20948"
+exclude :test_insert_record, "Updateable views are not currently supported, see https://github.com/cockroachdb/cockroach/issues/20948"
+exclude :test_update_record, "Updateable views are not currently supported, see https://github.com/cockroachdb/cockroach/issues/20948"
