3f0b515d2b0e831cfc5b685a0f17a12c99c8c5d0
Ali Ibrahim <aibrahim2k2@gmail.com>
2020-04-16 13:55:02 -0400

    Replace AR InvertibleMigrationTest default test
      * The test fails because type information is stripped from string column
        default values when the default is changed in the database. As a result new,
        unsaved records won't have default values.
      * We can still verify the desired behavior by persisting the test records.
        When ActiveRecord fetches the records from the database, they'll have their
        default values.

--- test/excludes/ActiveRecord/InvertibleMigrationTest.rb
+++ test/excludes/ActiveRecord/InvertibleMigrationTest.rb
@@ -1,1 +1,2 @@
 exclude :test_migrate_enable_and_disable_extension, "CockroachDB doesn't support enabling/disabling extensions."
+exclude :test_migrate_revert_change_column_default, "The test fails because type information is stripped from string column default values when the default is changed in the database. Possibly caused by https://github.com/cockroachdb/cockroach/issues/47285."
