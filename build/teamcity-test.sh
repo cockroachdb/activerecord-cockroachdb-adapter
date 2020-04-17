55830dbe3aa75e60300cad0c362184374cec9c2e
Rafi Shamim <rafi@cockroachlabs.com>
2020-04-17 14:00:18 -0400

    Run test cases separately in CI
    It seems that the CI build is hanging. In order to debug this, we now
    run each test separately.

--- build/teamcity-test.sh
+++ build/teamcity-test.sh
@@ -3,13 +3,18 @@
 set -euox pipefail
 
 # Download CockroachDB
-VERSION=v20.1.0-rc.2
-wget -qO- https://binaries.cockroachdb.com/cockroach-$VERSION.linux-amd64.tgz | tar  xvz
-readonly COCKROACH=./cockroach-$VERSION.linux-amd64/cockroach
+#VERSION=v20.1.0-rc.1
+#wget -qO- https://binaries.cockroachdb.com/cockroach-$VERSION.linux-amd64.tgz | tar  xvz
+#readonly COCKROACH=./cockroach-$VERSION.linux-amd64/cockroach
+
+mkdir -p ./cockroach
+wget -q -O cockroach/cockroach https://edge-binaries.cockroachdb.com/cockroach/cockroach.linux-gnu-amd64.LATEST
+chmod +x ./cockroach/cockroach
+export PATH=./cockroach/:$PATH
 
 # Make sure cockroach can be found on the path. This is required for the
 # ActiveRecord Rakefile that rebuilds the test database.
-export PATH=./cockroach-$VERSION.linux-amd64/:$PATH
+#export PATH=./cockroach-$VERSION.linux-amd64/:$PATH
 readonly urlfile=cockroach-url
 
 run_cockroach() {
@@ -44,13 +49,29 @@ bundle install
 
 run_cockroach
 
-if ! (RUBYOPT="-W0" TESTOPTS="-v" bundle exec rake test); then
-    echo "Tests failed"
-    HAS_FAILED=1
-else
-    echo "Tests passed"
-    HAS_FAILED=0
-fi
+source ./build/test_cases.sh
+
+HAS_FAILED=0
+
+for test_file in ${COCKROACH_TEST_CASES[@]}; do
+    echo "Running: $test_file"
+    if ! (RUBYOPT="-W0" TESTOPTS="-v" TEST_FILES="$test_file" bundle exec rake test); then
+        echo "Failed: $test_file"
+        HAS_FAILED=1
+    else
+        echo "Passed: $test_file"
+    fi
+done
+
+for test_file in ${AR_TEST_CASES[@]}; do
+    echo "Running: $test_file"
+    if ! (RUBYOPT="-W0" TESTOPTS="-v" TEST_FILES_AR="$test_file" bundle exec rake test); then
+        echo "Failed: $test_file"
+        HAS_FAILED=1
+    else
+        echo "Passed: $test_file"
+    fi
+done
 
 # Attempt a clean shutdown for good measure. We'll force-kill in the
 # exit trap if this script fails.
@@ -58,5 +79,6 @@ cockroach quit --insecure
 trap - EXIT
 
 if [ $HAS_FAILED -eq 1 ]; then
+  echo "Tests failed"
   exit 1
 fi
