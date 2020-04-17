df34a92c1c941581c9bac8315b9afd3f34469496
Ali Ibrahim <aibrahim2k2@gmail.com>
2020-04-17 15:24:31 -0400

    Skip AR Transaction QueryCanceled test
      * It tries to use pg_cancel_backend() which is not supported by CockroachDB.

--- test/excludes/ActiveRecord/PostgresqlTransactionTest.rb
+++ test/excludes/ActiveRecord/PostgresqlTransactionTest.rb
@@ -1,1 +1,2 @@
 exclude :test_raises_LockWaitTimeout_when_lock_wait_timeout_exceeded, "The test tries to set lock_timeout, but lock_timeout is not supported by CockroachDB."
+exclude :test_raises_QueryCanceled_when_canceling_statement_due_to_user_request, "CockroachDB doesn't support pg_cancel_backend()."
