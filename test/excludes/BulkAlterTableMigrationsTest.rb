c6617816de5c22cc161df18f2f7b1bcbb2415629
Marla Brizel Zeschin <marlabrizel@gmail.com>
2020-04-22 12:10:12 -0600

    Add CDB-specific BulkAlterTableMigrationsTest

---
+++ test/excludes/BulkAlterTableMigrationsTest.rb
@@ -0,0 +1,3 @@
+exclude :test_adding_indexes, "See test/cases/migration_test.rb for CockroachDB-specific test case"
+exclude :test_changing_columns, "See test/cases/migration_test.rb for CockroachDB-specific test case"
+exclude :test_adding_multiple_columns, "See test/cases/migration_test.rb for CockroachDB-specific test case"
