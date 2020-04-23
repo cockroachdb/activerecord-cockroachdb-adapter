c9e841997fc60d54bd2058598df563cfebbb41eb
Marla Brizel Zeschin <marlabrizel@gmail.com>
2020-04-23 16:34:31 -0600

    exclude decimal test
    CockroachDB uses 64-bit signed integers, whereas the default for PG is 32-bit. Active Record does not currently handle 64-bit integers.

--- test/excludes/MigrationTest.rb
+++ test/excludes/MigrationTest.rb
@@ -1,2 +1,3 @@
 exclude :test_create_table_with_query, "Two columns are created in CockroachDB since this query does not create a pk"
 exclude :test_create_table_with_query_from_relation, "Two columns are created in CockroachDB since this query does not create a pk"
+exclude :test_add_table_with_decimals, "CockroachDB uses 64-bit signed integers, whereas the default for PG is 32-bit. The Rails test does not accommodate the 64-bit case"
