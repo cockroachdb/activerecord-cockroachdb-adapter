0bb8a93adcf7aee6258100b317d851d7ca35f21f
Rafi Shamim <rafi@cockroachlabs.com>
2020-07-30 11:40:19 -0400

    Exclude flaky test_bulk_insert_with_a_multi_statement_query_in_a_nested_transaction

--- test/excludes/FixturesTest.rb
+++ test/excludes/FixturesTest.rb
@@ -1,1 +1,2 @@
 exclude :test_bulk_insert_multiple_table_with_a_multi_statement_query, "Skipping until we can triage further. See https://github.com/cockroachdb/activerecord-cockroachdb-adapter/issues/48"
+exclude :test_bulk_insert_with_a_multi_statement_query_in_a_nested_transaction, "Skipping until we can triage further. See https://github.com/cockroachdb/activerecord-cockroachdb-adapter/issues/48"
