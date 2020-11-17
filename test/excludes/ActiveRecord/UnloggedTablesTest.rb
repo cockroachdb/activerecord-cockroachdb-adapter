92d81ecf6efbc5b8e1b7d899aef76d5a84d4336f
fabricio.albarnaz <fabricio.albarna@gmail.com>
2020-11-17 10:38:12 -0300

    Fix exclude file path

---
+++ test/excludes/ActiveRecord/UnloggedTablesTest.rb
@@ -0,0 +1,2 @@
+exclude :test_gracefully_handles_temporary_tables, "This override can be removed after it be fix. Related https://github.com/cockroachdb/cockroach/issues/56656"
+exclude :test_unlogged_in_test_environment_when_unlogged_setting_enabled, "This override can be removed after it be fix. Related https://github.com/cockroachdb/cockroach/issues/56656"
