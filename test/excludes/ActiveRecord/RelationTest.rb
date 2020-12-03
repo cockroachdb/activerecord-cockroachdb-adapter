120938142f05ab182c4ecf5c349aba243b6bc0be
albarnaz <fabricio.albarnaz@gmail.com>
2020-12-03 16:29:40 -0300

    Fix on exclude files to skip tests correctly (#152)
    * Fix on exclude files to skip tests correctly
    Some tests are not skipped correctly.
    
    Co-authored-by: fabricio.albarnaz <fabricio.albarna@gmail.com>

---
+++ test/excludes/ActiveRecord/RelationTest.rb
@@ -0,0 +1,2 @@
+exclude :test_relation_with_annotation_filters_sql_comment_delimiters, "This test is overridden for CockroachDB because this adapter adds quotes to numeric values."
+exclude :test_relation_with_annotation_includes_comment_in_to_sql, "This test is overridden for CockroachDB because this adapter adds quotes to numeric values."
--- test/excludes/RelationTest.rb
+++ test/excludes/RelationTest.rb
@@ -1,3 +1,1 @@
 exclude :test_finding_with_sanitized_order, "Skipping until we can triage further. See https://github.com/cockroachdb/activerecord-cockroachdb-adapter/issues/48"
-exclude :test_relation_with_annotation_filters_sql_comment_delimiters, "This test is overridden for CockroachDB because this adapter adds quotes to numeric values."
-exclude :test_relation_with_annotation_includes_comment_in_to_sql, "This test is overridden for CockroachDB because this adapter adds quotes to numeric values."
