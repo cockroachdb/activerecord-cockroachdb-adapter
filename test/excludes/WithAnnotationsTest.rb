b6420fa7a81d979c3c79b52144ee9b8b01d187f2
fabricio.albarnaz <fabricio.albarna@gmail.com>
2020-12-09 16:22:36 -0300

    Update exclude comment

--- test/excludes/WithAnnotationsTest.rb
+++ test/excludes/WithAnnotationsTest.rb
@@ -1,1 +1,1 @@
-exclude :test_belongs_to_with_annotation_includes_a_query_comment, "Skip"
+exclude :test_belongs_to_with_annotation_includes_a_query_comment, "This test is overrided to fix the fixtures sequence. See test/cases/associations_test.rb"
