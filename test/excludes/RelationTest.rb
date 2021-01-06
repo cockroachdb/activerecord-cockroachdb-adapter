58c350a3240a9e9a4f8e55d90c02df7e5930d356
Rafi Shamim <rafi@cockroachlabs.com>
2021-01-05 21:06:04 -0500

    Override test_finding_with_subquery_with_eager_loading_in_from
    The test relies on deterministic order of SELECT results, but did not
    include an ORDER BY

--- test/excludes/RelationTest.rb
+++ test/excludes/RelationTest.rb
@@ -1,1 +1,2 @@
 exclude :test_finding_with_sanitized_order, "Skipping until we can triage further. See https://github.com/cockroachdb/activerecord-cockroachdb-adapter/issues/48"
+exclude :test_finding_with_subquery_with_eager_loading_in_from, "Overridden because test depends on ordering of results."
