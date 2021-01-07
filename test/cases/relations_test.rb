1ee11556c71acf48201ce35d3aac51236de878f3
Rafi Shamim <rafi@cockroachlabs.com>
2021-01-07 12:57:44 -0500

    Fix relations_test override
    It has to be namespaced under the CockroachDB module

--- test/cases/relations_test.rb
+++ test/cases/relations_test.rb
@@ -4,13 +4,15 @@ require "cases/helper"
 require "models/post"
 require "models/comment"
 
-class RelationTest < ActiveRecord::TestCase
-  fixtures :posts, :comments
+module CockroachDB
+  class RelationTest < ActiveRecord::TestCase
+    fixtures :posts, :comments
 
-  def test_finding_with_subquery_with_eager_loading_in_from
-    relation = Comment.includes(:post).where("posts.type": "Post").order(:id)
-    assert_equal relation.to_a, Comment.select("*").from(relation).order(:id).to_a
-    assert_equal relation.to_a, Comment.select("subquery.*").from(relation).order(:id).to_a
-    assert_equal relation.to_a, Comment.select("a.*").from(relation, :a).order(:id).to_a
+    def test_finding_with_subquery_with_eager_loading_in_from
+      relation = Comment.includes(:post).where("posts.type": "Post").order(:id)
+      assert_equal relation.to_a, Comment.select("*").from(relation).order(:id).to_a
+      assert_equal relation.to_a, Comment.select("subquery.*").from(relation).order(:id).to_a
+      assert_equal relation.to_a, Comment.select("a.*").from(relation, :a).order(:id).to_a
+    end
   end
 end
