50ee2f266e6613804eafc0b79fe98f057e6e0e83
Keith Doggett <keith.doggett887@gmail.com>
2021-07-13 12:19:58 -0400

    CockroachDB v21 support for ActiveRecord 6.x

--- test/excludes/SchemaDumperDefaultsTest.rb
+++ test/excludes/SchemaDumperDefaultsTest.rb
@@ -1,1 +1,1 @@
-exclude :test_schema_dump_with_float_column_infinity_default, "Skipping until we can triage further. See https://github.com/cockroachdb/activerecord-cockroachdb-adapter/issues/48"
+exclude :test_schema_dump_with_column_infinity_default, "Skipping until we can triage further. See https://github.com/cockroachdb/activerecord-cockroachdb-adapter/issues/48"
