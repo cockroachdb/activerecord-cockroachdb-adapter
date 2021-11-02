8fcf7ba6b13e6ed239445515a70edb0e7ac7fcce
Keith Doggett <keith.doggett887@gmail.com>
2021-11-02 11:51:35 -0400

    bump to rc3

--- build/teamcity-test.sh
+++ build/teamcity-test.sh
@@ -3,7 +3,7 @@
 set -euox pipefail
 
 # Download CockroachDB
-VERSION=v21.2.0-rc.2
+VERSION=v21.2.0-rc.3
 wget -qO- https://binaries.cockroachdb.com/cockroach-$VERSION.linux-amd64.tgz | tar  xvz
 readonly COCKROACH=./cockroach-$VERSION.linux-amd64/cockroach
 
