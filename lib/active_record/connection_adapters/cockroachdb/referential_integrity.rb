4a2d9dc8e13fff43f132d2ac809b53e9364223c4
Keith Doggett <keith.doggett887@gmail.com>
2021-12-15 14:46:00 -0500

    Handle prefixes and suffixes in disable_referential_integrity

--- lib/active_record/connection_adapters/cockroachdb/referential_integrity.rb
+++ lib/active_record/connection_adapters/cockroachdb/referential_integrity.rb
@@ -20,17 +20,31 @@ module ActiveRecord
 
           yield
 
-          foreign_keys.each do |foreign_key|
-            begin
-              add_foreign_key(foreign_key.from_table, foreign_key.to_table, **foreign_key.options)
-            rescue ActiveRecord::StatementInvalid => error
-              if error.cause.class == PG::DuplicateObject
-                # This error is safe to ignore because the yielded caller
-                # already re-added the foreign key constraint.
-              else
-                raise error
+          # Prefixes and suffixes are added in add_foreign_key
+          # in AR7+ so we need to temporarily disable them here,
+          # otherwise prefixes/suffixes will be erroneously added.
+          old_prefix = ActiveRecord::Base.table_name_prefix
+          old_suffix = ActiveRecord::Base.table_name_suffix
+
+          ActiveRecord::Base.table_name_prefix = ""
+          ActiveRecord::Base.table_name_suffix = ""
+
+          begin
+            foreign_keys.each do |foreign_key|
+              begin
+                add_foreign_key(foreign_key.from_table, foreign_key.to_table, **foreign_key.options)
+              rescue ActiveRecord::StatementInvalid => error
+                if error.cause.class == PG::DuplicateObject
+                  # This error is safe to ignore because the yielded caller
+                  # already re-added the foreign key constraint.
+                else
+                  raise error
+                end
               end
             end
+          ensure
+            ActiveRecord::Base.table_name_prefix = old_prefix
+            ActiveRecord::Base.table_name_prefix = old_suffix
           end
         end
       end
