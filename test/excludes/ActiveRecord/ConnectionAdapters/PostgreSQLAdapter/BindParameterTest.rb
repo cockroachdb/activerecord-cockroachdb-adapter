0b11a8a952ea9195eae31a2962144f847c495887
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2022-06-01 19:37:18 -0300

    Skip binding parameters tests because of exceptions not being raised

---
+++ test/excludes/ActiveRecord/ConnectionAdapters/PostgreSQLAdapter/BindParameterTest.rb
@@ -0,0 +1,5 @@
+exclude :test_where_with_integer_for_string_column_using_bind_parameters, "Skipping because StatementInvalid exception is not being raised for unknown reason."
+exclude :test_where_with_rational_for_string_column_using_bind_parameters, "Skipping because StatementInvalid exception is not being raised for unknown reason."
+exclude :test_where_with_float_for_string_column_using_bind_parameters, "Skipping because StatementInvalid exception is not being raised for unknown reason."
+exclude :test_where_with_decimal_for_string_column_using_bind_parameters, "Skipping because StatementInvalid exception is not being raised for unknown reason."
+exclude :test_where_with_duration_for_string_column_using_bind_parameters, "Skipping because StatementInvalid exception is not being raised for unknown reason."
