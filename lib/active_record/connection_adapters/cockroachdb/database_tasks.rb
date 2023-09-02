cb1313bdf9aece685d0cd82ef0b63bc853124c51
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2023-09-02 10:13:07 -0500

    feat: support schema search path
    Fixes #276

--- lib/active_record/connection_adapters/cockroachdb/database_tasks.rb
+++ lib/active_record/connection_adapters/cockroachdb/database_tasks.rb
@@ -10,37 +10,44 @@ module ActiveRecord
               "https://github.com/cockroachdb/activerecord-cockroachdb-adapter/issues/new"
           end
 
-          case ActiveRecord.dump_schemas
-          when :all, String
-            raise  "Custom schemas are not supported in CockroachDB. " \
-              "See https://github.com/cockroachdb/cockroach/issues/26443."
-          when :schema_search_path
-            if configuration_hash[:schema_search_path]
-              raise  "Custom schemas are not supported in CockroachDB. " \
-                "See https://github.com/cockroachdb/cockroach/issues/26443."
+          # "See https://github.com/cockroachdb/cockroach/issues/26443."
+          search_path =
+            case ActiveRecord.dump_schemas
+            when :schema_search_path
+              configuration_hash[:schema_search_path]
+            when :all
+              nil
+            when String
+              ActiveRecord.dump_schemas
             end
-          end
 
           conn = ActiveRecord::Base.connection
-          File.open(filename, "w") do |file|
-            %w(SCHEMAS TYPES).each do |object_kind|
-              ActiveRecord::Base.connection.execute("SHOW CREATE ALL #{object_kind}").each_row { file.puts _1 }
-            end
-
-            ignore_tables = ActiveRecord::SchemaDumper.ignore_tables.to_set
-
-            conn.execute("SHOW CREATE ALL TABLES").each_row do |(sql)|
-              if sql.start_with?("CREATE")
-                table_name = sql[/CREATE TABLE (?:.*?\.)?\"?(.*?)[\" ]/, 1]
-                next if ignore_tables.member?(table_name)
-              elsif sql.start_with?("ALTER")
-                table_name = sql[/ALTER TABLE (?:.*?\.)?\"?(.*?)[\" ]/, 1]
-                ref_table_name = sql[/REFERENCES (?:.*?\.)?\"?(.*?)[\" ]/, 1]
-                next if ignore_tables.member?(table_name) || ignore_tables.member?(ref_table_name)
+          begin
+            old_search_path = conn.schema_search_path
+            conn.schema_search_path = search_path
+            File.open(filename, "w") do |file|
+              %w(SCHEMAS TYPES).each do |object_kind|
+                ActiveRecord::Base.connection.execute("SHOW CREATE ALL #{object_kind}").each_row { file.puts _1 }
               end
 
-              file.puts sql
+              ignore_tables = ActiveRecord::SchemaDumper.ignore_tables.to_set
+
+              conn.execute("SHOW CREATE ALL TABLES").each_row do |(sql)|
+                if sql.start_with?("CREATE")
+                  table_name = sql[/CREATE TABLE (?:.*?\.)?\"?(.*?)[\" ]/, 1]
+                  next if ignore_tables.member?(table_name)
+                elsif sql.start_with?("ALTER")
+                  table_name = sql[/ALTER TABLE (?:.*?\.)?\"?(.*?)[\" ]/, 1]
+                  ref_table_name = sql[/REFERENCES (?:.*?\.)?\"?(.*?)[\" ]/, 1]
+                  next if ignore_tables.member?(table_name) || ignore_tables.member?(ref_table_name)
+                end
+
+                file.puts sql
+              end
+              file.puts "SET seach_path TO #{conn.schema_search_path};\n\n"
             end
+          ensure
+            conn.schema_search_path = old_search_path
           end
         end
 
