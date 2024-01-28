5abb32516e8dcdc0544216b62b050d2739db213a
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2024-01-28 14:49:28 -0300

    chore: remove noisy outdated overrides

--- test/excludes/SchemaDumperDefaultsTest.rb
+++ test/excludes/SchemaDumperDefaultsTest.rb
@@ -1,3 +1,1 @@
 exclude :test_schema_dump_with_column_infinity_default, "Skipping until we can triage further. See https://github.com/cockroachdb/activerecord-cockroachdb-adapter/issues/48"
-exclude :test_schema_dump_defaults_with_universally_supported_types, "Re-implementing ourselves because we need CockroachDB specific methods."
-exclude :test_schema_dump_with_text_column, "Re-implementing ourselves because we need CockroachDB specific methods."
