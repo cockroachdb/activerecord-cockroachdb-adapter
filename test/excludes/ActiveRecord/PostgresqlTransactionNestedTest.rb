8c1841b306212481be28e76aec03730a73bc2d49
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2024-08-27 16:01:27 +0200

    Fix various tests (#333)
    - Remove `database_exists?` method from `CockroachDBAdapter`, the
      original method is fine.
    - Exclude a test that will be fixed in Rails 7.2

--- test/excludes/ActiveRecord/PostgresqlTransactionNestedTest.rb
+++ test/excludes/ActiveRecord/PostgresqlTransactionNestedTest.rb
@@ -1,2 +1,14 @@
-exclude :test_deadlock_raises_Deadlocked_inside_nested_SavepointTransaction, "Causes CI to hand. Skip while debugging."
-exclude :test_unserializable_transaction_raises_SerializationFailure_inside_nested_SavepointTransaction, "Causes CI to hand. Skip while debugging."
+
+# > In CRDB SERIALIZABLE, reads block on in-progress writes for
+# > as long as those writes are in progress. However, PG does
+# > not have this "read block on write" behavior, and so rather
+# > than allowing the left-hand-side to execute, it must instead
+# > abort that transaction. Both are valid ways to implement SERIALIZABLE.
+#
+# See discussion: https://github.com/cockroachdb/activerecord-cockroachdb-adapter/pull/333
+message = "SERIALIZABLE transactions are different in CockroachDB."
+
+exclude :test_deadlock_raises_Deadlocked_inside_nested_SavepointTransaction, message
+exclude :test_unserializable_transaction_raises_SerializationFailure_inside_nested_SavepointTransaction, message
+exclude :test_SerializationFailure_inside_nested_SavepointTransaction_is_recoverable, message
+exclude :test_deadlock_inside_nested_SavepointTransaction_is_recoverable, message
