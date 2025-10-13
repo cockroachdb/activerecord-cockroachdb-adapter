e27240ed43300946e8e535c291413f9b5c771c0a
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2025-10-13 13:46:18 +0200

    fix(tests): clear query cache on insert
    Since [rails pull request #52428][1], `#execute_batch` does not trigger
    a cache clear anymore. However, `#insert_fixtures_set` relies on that
    clear to ensure consistency. In the postgresql adapter, this is ensured
    by a call to `#execute` rather than `#execute_batch` in
    `#disable_referential_integrity`. Since we are not always calling
    `#disable_referential_integrity`, we need to ensure that the cache is
    cleared when running our statements by calling `#execute` instead of
    `#execute_batch`.
    
    [1]: https://github.com/rails/rails/pull/52428
    
    Signed-off-by: Ulysse Buonomo <buonomo.ulysse@gmail.com>

--- lib/active_record/connection_adapters/cockroachdb/database_statements.rb
+++ lib/active_record/connection_adapters/cockroachdb/database_statements.rb
@@ -22,15 +22,26 @@ module ActiveRecord
         def insert_fixtures_set(fixture_set, tables_to_delete = [])
           fixture_inserts = build_fixture_statements(fixture_set)
           table_deletes = tables_to_delete.map { |table| "DELETE FROM #{quote_table_name(table)}" }
-          statements = table_deletes + fixture_inserts
+          statements = (table_deletes + fixture_inserts).join(";")
+
+          # Since [rails pull request #52428][1], `#execute_batch` does not
+          # trigger a cache clear anymore. However, `#insert_fixtures_set`
+          # relies on that clear to ensure consistency. In the postgresql
+          # adapter, this is ensured by a call to `#execute` rather than
+          # `#execute_batch` in `#disable_referential_integrity`. Since
+          # we are not always calling `#disable_referential_integrity`,
+          # we need to ensure that the cache is cleared when running
+          # our statements by calling `#execute` instead of `#execute_batch`.
+          #
+          # [1]: https://github.com/rails/rails/pull/52428
 
           begin # much faster without disabling referential integrity, worth trying.
             transaction(requires_new: true) do
-              execute_batch(statements, "Fixtures Load")
+              execute(statements, "Fixtures Load")
             end
           rescue
             disable_referential_integrity do
-              execute_batch(statements, "Fixtures Load")
+              execute(statements, "Fixtures Load")
             end
           end
         end
