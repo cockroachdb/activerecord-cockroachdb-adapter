5f7b588f1ffdfd789b4a60dc80a30992f85298c7
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2025-10-30 16:05:17 +0100

    fix(test): coerced value for generate column

--- test/excludes/PostgresqlVirtualColumnTest.rb
+++ test/excludes/PostgresqlVirtualColumnTest.rb
@@ -1,2 +1,17 @@
+require "support/copy_cat"
+
 exclude :test_build_fixture_sql, "Skipping because CockroachDB cannot write directly to computed columns."
 exclude :test_schema_dumping, "Replaced with local version"
+
+# CRDB doesn't support implicit casts.
+# See https://github.com/cockroachdb/cockroach/issues/75101
+#
+# From: "ASCII(name)"
+#   To: "ASCII(name)""::string"
+CopyCat.copy_methods(self, self, :test_change_table_without_stored_option) do
+  def on_str(node)
+    return unless node in [:str, "ASCII(name)"]
+
+    insert_after(node.loc.expression, '"::string"')
+  end
+end
