86d06294f4db8864b47d382514f71011c2a893cc
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2025-11-19 18:53:03 +0100

    fix: ensure type_map gets sql type details
    In our adapter, we rely on the sql type string for
    spatial details to properly map the type to the correct
    ActiveRecord type. It was not shared.

--- lib/active_record/connection_adapters/cockroachdb/quoting.rb
+++ lib/active_record/connection_adapters/cockroachdb/quoting.rb
@@ -59,6 +59,21 @@ module ActiveRecord
           # This is tested by `BasicsTest#test_default_in_local_time`.
           super + value.strftime("%z")
         end
+
+        # NOTE: This method should be private in future rails versions.
+        #   Hence we should also make it private then.
+        #
+        # See https://github.com/rails/rails/blob/v8.1.1/activerecord/lib/active_record/connection_adapters/postgresql/quoting.rb#L190
+        def lookup_cast_type(sql_type)
+          type_map.lookup(
+            # oid
+            query_value("SELECT #{quote(sql_type)}::regtype::oid", "SCHEMA").to_i,
+            # fmod, not needed.
+            nil,
+            # details needed for `..::CockroachDB::OID::Spatial` (e.g. `geometry(point,3857)`)
+            sql_type
+          )
+        end
       end
     end
   end
